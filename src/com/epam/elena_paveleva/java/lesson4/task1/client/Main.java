3aa90f04ae46be8986706c0460a67d122a97d97d
Evgenii Pavelev <johnlang.langepas@gmail.com>
2019-05-10 20:08:00 +0300

    2nd task HW4

--- src/com/epam/elena_paveleva/java/lesson4/task1/client/Main.java
+++ src/com/epam/elena_paveleva/java/lesson4/task1/client/Main.java
@@ -1,10 +1,15 @@
 package com.epam.elena_paveleva.java.lesson4.task1.client;
 
+import com.epam.elena_paveleva.java.lesson4.task1.impl.CollectionService;
 import com.epam.elena_paveleva.java.lesson4.task1.impl.DynamicArray;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        DynamicArray<Integer> dynArr = new DynamicArray<Integer>(3);
+        DynamicArray<Integer> dynArr = new DynamicArray<>(3);
         dynArr.add(1);
         dynArr.add(3);
         dynArr.add(4);
@@ -21,5 +26,16 @@ public class Main {
         dynArr2.add("test4");
         dynArr2.remove(1);
         System.out.println(dynArr2.toString());
+
+        //task2
+        Integer[] arr = new Integer[1_000_000];
+        CollectionService.generateArray(arr);
+        ArrayList arrayList = new ArrayList<>(Arrays.asList(arr));
+        Collections.shuffle(arrayList);
+        System.out.println("All elements unique: " + CollectionService.isAllUnique(arrayList));
+        System.out.println("Min element: " + Collections.min(arrayList));
+        CollectionService.removeOddElements(arrayList);
+        Collections.sort(arrayList);
+        System.out.println("second min element: " + arrayList.get(arrayList.size() - 2));
     }
 }
