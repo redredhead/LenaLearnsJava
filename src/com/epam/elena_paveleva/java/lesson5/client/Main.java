fea71a14befcd9e5a7ec7f3ea941f977f6a01027
redredhead <flary2007@mail.ru>
2019-08-25 22:41:15 +0300

    task 4 done

--- src/com/epam/elena_paveleva/java/lesson5/client/Main.java
+++ src/com/epam/elena_paveleva/java/lesson5/client/Main.java
@@ -3,17 +3,32 @@ package com.epam.elena_paveleva.java.lesson5.client;
 import com.epam.elena_paveleva.java.lesson5.files.FileService;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Path path = Paths.get("C:\\Users\\Evgenii\\Downloads");
-        List<Path> files;
-        int number = 3;
+    public static void main(String[] args) {
 
-        switch (number) {
+        if (validateInput(args)) {
+            try {
+                int task = Integer.parseInt(args[0]);
+                Path path = Paths.get(args[1]);
+                execute(task, path);
+            } catch (IOException e) {
+                System.out.println("Something went wrong");
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Input not valid");
+        }
+    }
+
+    private static void execute(int task, Path path) throws IOException {
+        List<Path> files;
+        switch (task) {
             case 1:
                 FileService.findMaxSymbolFile(FileService.listFiles(path), 's');
                 break;
@@ -24,8 +39,28 @@ public class Main {
                 break;
 
             case 3:
-                files = FileService.listDirectories(path);
                 FileService.printAverageFileSizeForAllSubfolders(path);
+                break;
+
+            case 4:
+                FileService.printPathsByName(path);
+                break;
         }
+    }
+
+    private static boolean validateInput(String[] args) {
+        int task;
+        Path path;
+        if (args.length != 2) {
+            return false;
+        }
+        try {
+            task = Integer.parseInt(args[0]);
+            path = Paths.get(args[1]);
+        } catch (NumberFormatException | InvalidPathException e) {
+            return false;
+        }
+
+        return Files.exists(path) && Files.isDirectory(path) && task < 5;
     }
 }
--- src/com/epam/elena_paveleva/java/lesson5/files/FileService.java
+++ src/com/epam/elena_paveleva/java/lesson5/files/FileService.java
@@ -5,7 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -51,7 +53,6 @@ public class FileService {
         } else {
             System.out.println("No files containing " + symbol + " found");
         }
-
     }
 
     public static List<Path> sortBySize(List<Path> files) {
@@ -74,7 +75,6 @@ public class FileService {
         for (Path file : files) {
             System.out.println(((Files.size(file)) / (1024L * 1024L)) + " Mb   " + file);
         }
-
     }
 
     public static void printAverageFileSizeForAllSubfolders(Path path) throws IOException {
@@ -83,7 +83,6 @@ public class FileService {
         }
 
         List<Path> directories = FileService.listDirectories(path);
-
         for (Path directory : directories) {
             long avg = FileService.calculateAverageFileSizeInFolder(directory);
             if (avg == 0) {
@@ -91,6 +90,55 @@ public class FileService {
             } else {
                 System.out.println("Average size size of file for " + directory + " : " + (avg / 1024L) + " Kb");
             }
+        }
+    }
+
+    public static void printPathsByName(Path path) {
+        if (!Files.isDirectory(path)) {
+            throw new InvalidPathException("unable to get average file size", "path is not a directory");
+        }
+        try {
+            countFilesByName(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void countFilesByName(Path path) throws IOException {
+        class Counter {
+            private int files = 0;
+            private int directories = 0;
+
+            private Counter(int f, int d) {
+                files = f;
+                directories = d;
+            }
+        }
+
+        Map<Character, Counter> result = new HashMap<>();
+        try (Stream<Path> walk = Files.walk(path)) {
+            walk.forEach(x -> {
+                char ch = x.getFileName().toString().toUpperCase().charAt(0);
+                if (!result.containsKey(ch)) {
+                    if (Files.isRegularFile(x)) {
+                        result.put(ch, new Counter(1, 0));
+                    } else {
+                        result.put(ch, new Counter(0, 1));
+                    }
+                } else {
+                    if (Files.isRegularFile(x)) {
+                        result.get(ch).files++;
+                    } else {
+                        result.get(ch).directories++;
+                    }
+                }
+            });
+        } catch (IOException e) {
+            throw new IOException(e);
+        }
+
+        if (!result.isEmpty()) {
+            result.forEach((k, v) -> System.out.println(k + ": " + v.files + " files, " + v.directories + " directories"));
         }
     }
 
@@ -110,11 +158,9 @@ public class FileService {
                 }
                 return (sum / files.size());
             }
-
         } catch (IOException e) {
             throw new IOException(e);
         }
-
     }
 
     private static int countCharInFilename(Path file, char symbol) {
@@ -123,6 +169,6 @@ public class FileService {
             filename = filename.substring(0, filename.lastIndexOf('.'));
         }
         return (int) filename.chars().filter(ch -> ch == symbol).count();
-
     }
+
 }
